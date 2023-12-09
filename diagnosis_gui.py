afdb2b68863a39feafefde49aa8d2d0f3c7bef9b
sergio1599 <sergioq15@hotmail.com>
2023-12-09 14:35:32 -0500

    changes in the gui

--- diagnosis_gui.py
+++ diagnosis_gui.py
@@ -35,37 +35,51 @@ def on_diagnose_button_click():
 
     if diagnoses:
         result_label.config(
-            text="El paciente puede tener: " + ", ".join(diagnoses))
+            text="El paciente puede tener: " + ", ".join(diagnoses),
+            foreground="blue"
+        )
     else:
-        result_label.config(text="No se encontraron diagnósticos.")
+        result_label.config(
+            text="No se encontraron diagnósticos.",
+            foreground="red"
+        )
 
 
 # Crear la ventana principal
 window = tk.Tk()
 window.title("Sistema Experto de Diagnóstico Neurológico")
+window.geometry("500x300")
+
+# Crear estilo para mejorar la apariencia
+style = ttk.Style()
+style.configure('TButton', font=('Helvetica', 12))
+style.configure('TLabel', font=('Helvetica', 14), foreground='blue')
 
 # Crear widgets ComboBox
-symptoms = ['desorientacion', 'dificultad para resolver problemas', 'perdida de memoria',
-            'movimientos involuntarios', 'rigidez muscular', 'temblor en las manos',
-            'convulsiones', 'perdida de conciencia', 'aura visual',
-            'debilidad muscular', 'dolor muscular', 'dificultad para respirar',
-            'dolor de cabeza', 'nauseas', 'vomitos', 'agitacion', 'alucinaciones', 'cambios de personalidad']
+symptoms = [
+    'agitación', 'alucinaciones', 'aura visual', 'cambios de personalidad',
+    'convulsiones', 'desorientación', 'debilidad muscular', 'dificultad para respirar',
+    'dificultad para resolver problemas', 'dolor de cabeza', 'dolor muscular',
+    'movimientos involuntarios', 'nauseas', 'pérdida de conciencia',
+    'pérdida de memoria', 'rigidez muscular', 'temblor en las manos',
+    'vómitos'
+]
 
 symptom1_combobox = ttk.Combobox(window, values=symptoms)
-symptom1_combobox.pack()
+symptom1_combobox.pack(pady=10)
 
 symptom2_combobox = ttk.Combobox(window, values=symptoms)
-symptom2_combobox.pack()
+symptom2_combobox.pack(pady=10)
 
 symptom3_combobox = ttk.Combobox(window, values=symptoms)
-symptom3_combobox.pack()
+symptom3_combobox.pack(pady=10)
 
-run_button = tk.Button(window, text="Diagnosticar",
-                       command=on_diagnose_button_click)
-run_button.pack()
+run_button = ttk.Button(window, text="Diagnosticar",
+                        command=on_diagnose_button_click)
+run_button.pack(pady=20)
 
-result_label = tk.Label(window, text="")
-result_label.pack()
+result_label = ttk.Label(window, text="", foreground='blue')
+result_label.pack(pady=10)
 
 # Iniciar la aplicación
 window.mainloop()
